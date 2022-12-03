912784d5c75246c6e077015bb3d86af6bfd1ee66
elbiseu <elvis.sanchez.napoles@gmail.com>
2022-12-03 09:06:14 -0600

    Close pipe

--- transform.go
+++ transform.go
@@ -12,12 +12,10 @@ func Scale(img image.Image, x, y int) (image.Image, error) {
 	rgba := image.NewRGBA(image.Rect(0, 0, x, y))
 	draw.NearestNeighbor.Scale(rgba, rgba.Rect, img, img.Bounds(), draw.Over, nil)
 	r, w := io.Pipe()
+	defer r.Close()
+	defer w.Close()
 	go func() {
 		if err := jpeg.Encode(w, rgba, &jpeg.Options{Quality: 100}); err != nil {
-			log.Print(err)
-			return
-		}
-		if err := w.Close(); err != nil {
 			log.Print(err)
 			return
 		}
